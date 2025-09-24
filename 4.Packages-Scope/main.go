1ce26014dcfedbd02a3dfa23490b28e4c3d8ad3e
Arif Faisal <ariffaisal18.19@gmail.com>
2025-09-24 22:26:25 +0600

    Create main.go

---
+++ 4.Packages-Scope/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/mathlib"
+)
+
+func main() {
+	fmt.Println("Calling add on same package")
+	fmt.Println(add(3, 4)) // if you call this , must do "go run main.go sum.go"
+
+	fmt.Println("Calling add on different package")
+	fmt.Println(mathlib.Multiply(3, 4)) // can run with go run main.go
+	// fmt.Println(mathlib.money) // cant use small letter
+}
+
+func init() {
+	fmt.Println("Running init function which was inside main.go")
+}
