a77b60e5dd1fae90efb68ac3ec736fe8430b5d80
Arif Faisal <ariffaisal18.19@gmail.com>
2025-10-04 21:57:41 +0600

    Update sum.go

--- 4.Packages-Scope/sum.go
+++ 4.Packages-Scope/sum.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func add(a int, b int) int {
+func add(a int, b int) int { // add two numbers
 	return a + b
 }
 
 func init() {
 	fmt.Println("Running init function which was inside sum.go")
 }
+
